f9c64cea60b1cb5f2615d5fcb736ba2d9350c871
Giancarlo <cooper458.gm@gmail.com>
2026-02-11 16:59:02 +0100

    added initial blog structure

--- react-form/src/App.jsx
+++ react-form/src/App.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  const articles = [
+  const [articles, setArticles] = useState([
     { id: 1, title: "primo articolo" },
     { id: 2, title: "secondo articolo" },
     { id: 3, title: "terzo articolo" },
-  ];
-  return <h1>Blog</h1>;
+  ]);
+  return (
+    <div>
+      <h1>Blog</h1>|
+      <form>
+        <input type="text" placeholder="Titolo nuovo articolo..." />
+        <button type="submit">Aggiungi</button>
+      </form>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>{article.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
 export default App;
